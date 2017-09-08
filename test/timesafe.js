2cf61863a8b48dbe16fefe08e1945d63b89ffccd
Andy Gray <andy@whatsthescore.co.uk>
2017-09-08 16:17:42 +0100

    deposit test

--- test/timesafe.js
+++ test/timesafe.js
@@ -9,7 +9,7 @@ const TimeSafe = artifacts.require('./TimeSafe.sol')
  * Note these are ran in order and currently re-use the deployed contract
  */
 contract('TimeSafe', accounts => {
-  // const oneEther = 1000000000000000000
+  const oneEther = 1000000000000000000
 
   // TimeSafe is deployed with this date - see migrations
   const FirstJan2100 = 4102444800
@@ -60,5 +60,33 @@ contract('TimeSafe', accounts => {
         return instance.balances.call(accounts[0])
       })
       .then(result => assert.equal(result.valueOf(), 0))
+  })
+
+  it('should allow deposits from account 0 when locked', () => {
+    let timeSafeInstance
+    return TimeSafe.deployed()
+      .then(instance => {
+        timeSafeInstance = instance
+        return timeSafeInstance.deposit({from: accounts[0], value: oneEther})
+      })
+      .then(tx => timeSafeInstance.depositsCount.call())
+      .then(result => assert.equal(result.valueOf(), 1, 'made 1 deposit'))
+      .then(() => timeSafeInstance.totalDeposits.call())
+      .then(result => assert.equal(result.valueOf(), oneEther, 'should be exactly 1 ether in the contract'))
+      .catch(ex => console.log(ex))
+  })
+
+  it('should sum mulitple deposits together from account 0 when locked', () => {
+    let timeSafeInstance
+    return TimeSafe.deployed()
+      .then(instance => {
+        timeSafeInstance = instance
+        return timeSafeInstance.deposit({from: accounts[0], value: oneEther})
+      })
+      .then(tx => timeSafeInstance.depositsCount.call())
+      .then(result => assert.equal(result.valueOf(), 2, 'made 2 deposits'))
+      .then(() => timeSafeInstance.totalDeposits.call())
+      .then(result => assert.equal(result.valueOf(), oneEther * 2, 'should be exactly 2 ether in the contract'))
+      .catch(ex => console.log(ex))
   })
 })
